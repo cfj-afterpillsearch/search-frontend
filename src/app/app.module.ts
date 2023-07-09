4a04c5f4068338663f861f3750217ce209dd7ee1
tanimuranaomichi <tanimuranaomichi@gmail.com>
2023-07-09 17:32:09 +0900

    ヘッダーをFlowbiteに置換

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { SearchComponent } from './search/search.component';
 import { MedicalInstitutionsComponent } from './medical-institutions/medical-institutions.component';
 import { AppRoutingModule } from './app-routing.module';
 import {HttpClientModule} from "@angular/common/http";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import {HttpClientModule} from "@angular/common/http";
     HomeComponent,
     SearchComponent,
     MedicalInstitutionsComponent,
+    HeaderComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
