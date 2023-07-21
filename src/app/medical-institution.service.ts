54f10de24de9bfaf5e16548f0335d68c20abd9bf
tanimuranaomichi <tanimuranaomichi@gmail.com>
2023-07-21 20:17:57 +0900

    フロントとバックをつなぐ

--- src/app/medical-institution.service.ts
+++ src/app/medical-institution.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Observable} from "rxjs";
-import {MedicalInstitution} from "./medical-institution";
+import {ApiResponse} from "./api-response";
 import {HttpClient} from "@angular/common/http";
 
 @Injectable({
@@ -9,9 +9,9 @@ import {HttpClient} from "@angular/common/http";
 export class MedicalInstitutionService {
   constructor(private http: HttpClient) { }
 
-  getMedicalInstitutionsByCurrentLocation(latitude: number, longitude: number): Observable<MedicalInstitution[]> {
-    const apiUrl = `/api/search/medical-institutions?latitude=${latitude}&longitude=${longitude}`;
-    return this.http.get<MedicalInstitution[]>(apiUrl);
+  getMedicalInstitutionsByCurrentLocation(latitude: number, longitude: number): Observable<ApiResponse> {
+    const apiUrl = `https://apiserver-ts4nreykda-an.a.run.app/api/v1/search/current-location/medical-institutions?latitude=${latitude}&longitude=${longitude}`;
+    return this.http.get<ApiResponse>(apiUrl);
   }
 
 }
