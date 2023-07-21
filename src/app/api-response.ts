17d376b050886f1f1b16cfb91d5811d9adccfca9
tanimuranaomichi <tanimuranaomichi@gmail.com>
2023-07-21 20:10:00 +0900

    インタフェースの追加

---
+++ src/app/api-response.ts
@@ -0,0 +1,27 @@
+import {MedicalInstitution} from "./medical-institution";
+
+interface MetaData {
+  itemsPerPage: number;
+  totalItems: number;
+  currentPage: number;
+  totalPages: number;
+  searchType: string;
+  latitude: number;
+  longitude: number;
+  address_todofuken: string;
+  address_shikuchoson: string;
+}
+
+interface Links {
+  current: string;
+  first: string;
+  prev: string | null;
+  next: string | null;
+  last: string;
+}
+
+export interface ApiResponse {
+  meta: MetaData;
+  links: Links;
+  results: MedicalInstitution[];
+}
