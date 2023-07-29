2e180e7fb93b6df1fd77d0b6b953d8ff7237bcdc
osappy <904929+osappy@users.noreply.github.com>
2023-07-29 14:43:10 +0900

    remove header compornent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './shared/layouts/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [RouterOutlet],
 })
 export class AppComponent implements OnInit {
   title = 'afterpill-search';
