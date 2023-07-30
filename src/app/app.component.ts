2f04d9f33a60e1d86f2da268513e4033bec3544f
osappy <904929+osappy@users.noreply.github.com>
2023-07-30 16:05:23 +0900

    fix routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
 })
 export class AppComponent implements OnInit {
   title = 'afterpill-search';
