979810d0d0cd867358ffd0c4903cce0ee8084cb3
osappy <904929+osappy@users.noreply.github.com>
2023-08-05 22:23:02 +0900

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { PrivacyComponent } from './features/privacy/privacy.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'privacy', component: PrivacyComponent },
   {
     path: 'medical-institutions',
     loadChildren: () =>
