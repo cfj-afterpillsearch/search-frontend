2b2160fb67a3569b1ef183e0eb62aa00f49e3b56
erikka-22 <iwasaki.pingoo@gmail.com>
2023-08-06 16:00:01 +0900

    色の整理

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,9 @@ module.exports = {
         turquoise: {
           300: '#0192A8'
         },
-        rose: {
-          450: '#ed7171'
-        },
         slate: {
           750: '#3e3e3e'
         },
-        gray: {
-          350: '#d9d9d9'
-        }
       }
     },
   },
