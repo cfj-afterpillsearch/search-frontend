a8e4f5ef4260af75234b387b87dd419b540cd142
erikka-22 <iwasaki.pingoo@gmail.com>
2023-08-14 22:04:34 +0900

    環境変数名を変更

--- src/env.d.ts
+++ src/env.d.ts
@@ -9,5 +9,5 @@ interface ImportMetaEnv {
    */
   readonly NG_APP_API_URL: string;
   readonly NG_APP_BASE_URL: string;
-  readonly GOOGLE_TAG_MANAGER_ID: string;
+  readonly NG_APP_GOOGLE_TAG_MANAGER_ID: string;
 }
