a8a11e6c050edfc7ef36ac545b90e2d7fda5a1df
tanimuranaomichi <tanimuranaomichi@gmail.com>
2024-01-20 18:57:18 +0900

    chore: config-conventionalの導入

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,6 @@
 module.exports = {
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [0, 'always', 100],
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'test', 'refactor', 'style', 'docs', 'perf', 'config', 'security', 'upgrade', 'plus', 'minus'],
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'revert']],
   },
 };
