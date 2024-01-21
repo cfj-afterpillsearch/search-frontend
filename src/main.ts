07a101f223179a190328a89073d3320b25875665
tanimuranaomichi <tanimuranaomichi@gmail.com>
2024-01-21 22:43:55 +0900

    リポジトリ全体に対してprettierを実行

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,13 @@ import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http'
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { environment } from 'src/environments/environment';
-import { GoogleTagManagerService } from 'angular-google-tag-manager'
+import { GoogleTagManagerService } from 'angular-google-tag-manager';
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule),
-        provideHttpClient(withInterceptorsFromDi()),
-        {provide: 'googleTagManagerId', useValue: environment.GTM_ID},
-        GoogleTagManagerService
-    ]
-})
-  .catch(err => console.error(err));
+  providers: [
+    importProvidersFrom(BrowserModule, AppRoutingModule, FormsModule),
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: 'googleTagManagerId', useValue: environment.GTM_ID },
+    GoogleTagManagerService,
+  ],
+}).catch((err) => console.error(err));
